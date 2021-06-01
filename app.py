6385ea7d9762747891692562daa7cec689a92b2e
mattj241 <londonm241@gmail.com>
2021-05-31 22:51:36 -0400

    Code cleanup and addition of stack-overflow contributions

--- app.py
+++ app.py
@@ -224,6 +224,7 @@ def venues():
 @app.route('/venues/search', methods=['POST'])
 def search_venues():
   user_query=request.form.get('search_term', '')
+  # Below line was grabbed from: https://stackoverflow.com/questions/40412034/flask-sqlalchemy-contains-ilike-producing-different-results
   venues = Venue.query.filter(Venue.name.ilike(r"%{}%".format(user_query)))
   data = []
   for venue in venues:
@@ -338,6 +339,7 @@ def artists():
 @app.route('/artists/search', methods=['POST'])
 def search_artists():
   user_query=request.form.get('search_term', '')
+  # Below line was grabbed from: https://stackoverflow.com/questions/40412034/flask-sqlalchemy-contains-ilike-producing-different-results
   artists = Artist.query.filter(Artist.name.ilike(r"%{}%".format(user_query)))
   data = []
   for artist in artists:
@@ -531,43 +533,6 @@ def shows():
       "start_time" : format_datetime(str(show.start_time))
     }
     data.append(temp_show.copy())
-
-  # data=[{
-  #   "venue_id": 1,
-  #   "venue_name": "The Musical Hop",
-  #   "artist_id": 4,
-  #   "artist_name": "Guns N Petals",
-  #   "artist_image_link": "https://images.unsplash.com/photo-1549213783-8284d0336c4f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80",
-  #   "start_time": "2019-05-21T21:30:00.000Z"
-  # }, {
-  #   "venue_id": 3,
-  #   "venue_name": "Park Square Live Music & Coffee",
-  #   "artist_id": 5,
-  #   "artist_name": "Matt Quevedo",
-  #   "artist_image_link": "https://images.unsplash.com/photo-1495223153807-b916f75de8c5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80",
-  #   "start_time": "2019-06-15T23:00:00.000Z"
-  # }, {
-  #   "venue_id": 3,
-  #   "venue_name": "Park Square Live Music & Coffee",
-  #   "artist_id": 6,
-  #   "artist_name": "The Wild Sax Band",
-  #   "artist_image_link": "https://images.unsplash.com/photo-1558369981-f9ca78462e61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=794&q=80",
-  #   "start_time": "2035-04-01T20:00:00.000Z"
-  # }, {
-  #   "venue_id": 3,
-  #   "venue_name": "Park Square Live Music & Coffee",
-  #   "artist_id": 6,
-  #   "artist_name": "The Wild Sax Band",
-  #   "artist_image_link": "https://images.unsplash.com/photo-1558369981-f9ca78462e61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=794&q=80",
-  #   "start_time": "2035-04-08T20:00:00.000Z"
-  # }, {
-  #   "venue_id": 3,
-  #   "venue_name": "Park Square Live Music & Coffee",
-  #   "artist_id": 6,
-  #   "artist_name": "The Wild Sax Band",
-  #   "artist_image_link": "https://images.unsplash.com/photo-1558369981-f9ca78462e61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=794&q=80",
-  #   "start_time": "2035-04-15T20:00:00.000Z"
-  # }]
   return render_template('pages/shows.html', shows=data)
 
 @app.route('/shows/create')
